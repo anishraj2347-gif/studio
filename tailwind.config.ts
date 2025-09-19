207f096ce2507fcb32f1c32eab1c10ae7576ef82
Anish Raj <koyena9798@gmail.com>
2025-09-19 06:46:56 +0000

    Let use found as Lexend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Alegreya', 'serif'],
-        headline: ['Alegreya', 'serif'],
+        body: ['Lexend', 'sans-serif'],
+        headline: ['Lexend', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
